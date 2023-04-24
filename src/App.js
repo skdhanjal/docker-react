18991c2fac12ee89d0f48eb3775e8a3a87e5f56c
Shashi Kumar <shashi.kumar@walmart.com>
2023-04-24 14:39:16 +0530

    fix tests

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>This is demo react page deployed using Docker!</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>This is demo react page deployed using Docker</h1>
       </header>
     </div>
   );
